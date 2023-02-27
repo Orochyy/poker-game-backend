a1bdec10dcd980bcd49e6adf4b9a1253930bab15
Orochyy <yura333619@gmail.com>
2023-02-27 16:27:15 +0200

    add room repo

--- backend/repo/room/room_repo.go
+++ backend/repo/room/room_repo.go
@@ -1,1 +1,68 @@
 package room
+
+import "poker-game-backend/backend/db"
+
+func FindRoomByID(id int) (*db.Room, bool) {
+	var room db.Room
+	result := db.DB.First(&room, id)
+	if result.RowsAffected == 0 {
+		return nil, false
+
+	}
+	return &room, true
+}
+
+func FindWaitingListsByRoomID(id int) (*[]db.WaitingList, bool) {
+	var waitingLists []db.WaitingList
+	db.DB.Where("room_id", id).Find(&waitingLists)
+	return &waitingLists, true
+}
+
+func UpdateRoom(room *db.Room) {
+	db.DB.Save(room)
+}
+
+func DeleteRoom(room *db.Room) {
+	db.DB.Where("id = ?", room.ID).Delete(room)
+}
+
+func DeleteWaitingListByUserID(userID int, waiting *db.WaitingList) {
+	db.DB.Where("user_id = ?", userID).Delete(waiting)
+}
+
+func DeleteWaitingListsByRoomID(roomID int) {
+	db.DB.Where("room_id = ?", roomID).Delete(&db.WaitingList{})
+}
+
+func GetWaitingListsByRoomID(roomID int) []db.WaitingList {
+	var waitingLists []db.WaitingList
+	db.DB.Where("room_id = ?", roomID).Find(&waitingLists)
+	return waitingLists
+}
+
+func IncreaseUserAmount(userID, roomID, amount int) {
+	var waitingList db.WaitingList
+	db.DB.Where("room_id = ? AND user_id = ?", roomID, userID).First(&waitingList)
+	waitingList.AvailableMoney = waitingList.AvailableMoney + amount
+	db.DB.Save(&waitingList)
+}
+
+func DecreaseUserAmount(userID, roomID, amount int) {
+	var waitingList db.WaitingList
+	db.DB.Where("room_id = ? AND user_id = ?", roomID, userID).First(&waitingList)
+	waitingList.AvailableMoney = waitingList.AvailableMoney - amount
+	db.DB.Save(&waitingList)
+}
+
+func UpdateUserAmountEndGame(userID, roomID int) {
+	// find user amount in waiting list and
+	// update money to "users" table
+	var waitingList db.WaitingList
+	db.DB.Where("room_id = ? AND user_id = ?", roomID, userID).First(&waitingList)
+
+	var user db.User
+	db.DB.Where("id = ?", userID).First(&user)
+
+	user.Money += waitingList.AvailableMoney
+	db.DB.Save(&user)
+}
