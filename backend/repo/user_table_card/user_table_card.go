9758a54cc25950cf638dc9efdc1a979ffb761899
Orochyy <yura333619@gmail.com>
2023-02-27 16:28:40 +0200

    add user table card repo

--- backend/repo/user_table_card/user_table_card.go
+++ backend/repo/user_table_card/user_table_card.go
@@ -1,1 +1,18 @@
 package user_table_card
+
+import "poker-game-backend/backend/db"
+
+func AddNewCard(tableID, userID, cardID uint) {
+	userCard := db.UsersTablesCard{
+		TableID: tableID,
+		UserID:  userID,
+		CardID:  cardID,
+	}
+	db.DB.Create(&userCard)
+}
+
+func FindCards(tableID, userID int) []db.UsersTablesCard {
+	var cards []db.UsersTablesCard
+	db.DB.Preload("Card").Where("table_id = ? AND user_id = ?", tableID, userID).Find(&cards)
+	return cards
+}
