1a87d1ee0fd3b37ea69b2d215a0cdefec73e1a51
Orochyy <yura333619@gmail.com>
2023-02-27 16:26:38 +0200

    create repo card

--- backend/repo/card/card.go
+++ backend/repo/card/card.go
@@ -1,1 +1,15 @@
 package card
+
+import "poker-game-backend/backend/db"
+
+func FindCardIDByNumberAndSuit(number, suit int) uint {
+	var card db.Card
+	db.DB.Where("number = ? AND suit = ?", number, suit).Find(&card)
+	return card.ID
+}
+
+func FindCardByNumberAndSuit(number, suit int) db.Card {
+	var card db.Card
+	db.DB.Where("number = ? AND suit = ?", number, suit).Find(&card)
+	return card
+}
