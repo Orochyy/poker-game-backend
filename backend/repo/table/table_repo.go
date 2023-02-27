39809dc7e0caf8998296be21c3bf7bdf3b282c4b
Orochyy <yura333619@gmail.com>
2023-02-27 16:28:12 +0200

    add table repo

--- backend/repo/table/table_repo.go
+++ backend/repo/table/table_repo.go
@@ -1,1 +1,32 @@
 package table
+
+import "poker-game-backend/backend/db"
+
+func CreateNewTable(userID uint, roomID uint, round int, pot int, done bool) uint {
+	table := db.Table{
+		Round:  round,
+		Pot:    pot,
+		Done:   done,
+		UserID: userID,
+		RoomID: roomID,
+	}
+
+	_ = db.DB.Create(&table).RowsAffected == 1
+	return table.ID
+}
+
+func UpdateTable(table *db.Table) {
+	db.DB.Save(table)
+}
+
+func FindTableByRoomID(roomID uint) db.Table {
+	var table db.Table
+	db.DB.Preload("Cards").Where("room_id = ?", roomID).First(&table)
+	return table
+}
+
+func GetTableByID(tableID int) db.Table {
+	var table db.Table
+	db.DB.Preload("Cards").Where("id = ?", tableID).First(&table)
+	return table
+}
