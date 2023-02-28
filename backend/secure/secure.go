b85727745536b0ff00a52c95f9b4ff0471fd36d3
Orochyy <yura333619@gmail.com>
2023-02-28 16:39:25 +0200

    add secure

--- backend/secure/secure.go
+++ backend/secure/secure.go
@@ -1,1 +1,40 @@
 package secure
+
+import (
+	"github.com/golang-jwt/jwt"
+	"golang.org/x/crypto/bcrypt"
+	"os"
+	"strconv"
+	"time"
+)
+
+func HashPassword(password string) (string, bool) {
+	b, err := bcrypt.GenerateFromPassword([]byte(password), 12)
+	return string(b), err == nil
+}
+
+func ComparePassword(plain, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(plain))
+	return err == nil
+}
+
+func GenerateToken(s string) (string, bool) {
+	expireTime, err := strconv.Atoi(os.Getenv("JWT_EXPIRE_IN_MINUTE"))
+	if err != nil {
+		expireTime = 30 // default: 30 minutes
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"usr": s,
+		"exp": time.Now().Add(time.Minute *
+			time.Duration(expireTime)).Unix(),
+	})
+	hmacSecret := os.Getenv("HMAC_SECRET_KEY")
+	tokenString, err := token.SignedString([]byte(hmacSecret))
+	return tokenString, err == nil
+}
+
+func GetJWTClaim(claim string, token interface{}) interface{} {
+	_token := token.(*jwt.Token)
+	claims := _token.Claims.(jwt.MapClaims)
+	return claims[claim]
+}
